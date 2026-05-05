2502f961d01ed929ef7d5ca8080efa986b3a836e
Lorenzzo Michelim <lorenzzomichelim@gmail.com>
2026-05-05 17:13:55 -0300

    Update script.js

--- listaDeTarefas/script.js
+++ listaDeTarefas/script.js
@@ -1,15 +1,17 @@
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
-function addTask(){
+function addTask() {
   const textItem = taskInput.value.trim();
-  if (textItem !== ''){
+  if (textItem !== '') {
     const newTask = document.createElement('li');
     newTask.innerHTML = `
-      <span> ${textItem} </span>
-      <button onclick=""> Concluir </button>
-      <button onclick=""> Editar </button>
-      <button onclick=""> Remover </button>
+      <span>${textItem}</span>
+      <div class="button-group">
+        <button onclick="this.parentElement.parentElement.classList.toggle('completed')">Concluir</button>
+        <button onclick="">Editar</button>
+        <button onclick="this.parentElement.parentElement.remove()">Remover</button>
+      </div>
     `;
     taskList.appendChild(newTask);
     taskInput.value = '';
